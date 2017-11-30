071b282964821c3f4f43745a91c5e059a4d3f69c
adenwebb21 <33032049+adenwebb21@users.noreply.github.com>
2017-11-30 18:13:59 +0000

    Added initial files for the actual scripting related to the assignment

---
+++ Assignment/roomdata.js
@@ -0,0 +1,46 @@
+var roomArray = [
+	{
+		title:'Introduction',
+		text:"You approach the town of Fandalin on horseback, riding in following rumours of a monster hidden among the townsfolk. Outside of the bounds of the town are a number of farms - no doubt supplying vegetables and meat to the markets situated within. You see a miserable looking man standing by the side of the road. <br\><br\> You had been sent to this town following reports of strange occurrences.",
+		choices:[
+			{
+				text:'Approach the man',
+				index:1
+			},
+      {
+				text:'Ride past into the town',
+				index:3
+			}
+		]
+	},
+	{
+		title:'Talking to Farmer',
+		text:"You slow your horse to a trot and then to a full stop, calling out to the man. He looks up and narrows his eyes at you with apparent suspicion. You can see that his clothes are fairly thin and ragged and a scraggly beard covered his chin and lower jaw.\
+    <br\><br\>'Was it you? Did you scare off my pigs? All you adventurers, can’t trust any of ya!'\
+    <br\><br\>You shake your head and try to calm the man, you explain that you are here to investigate exactly this kind of unusual activity. You ask him if there was anything he could tell you about the culprit. \
+    He pauses for a moment, scratching his chin.\
+    <br\><br\>'I mean they were long gone before I even got out of the door...'",
+		choices:[
+			{
+				text:'Ask the farmer about the pigs',
+				index:2
+			},
+			{
+				text:'Leave the farmer and continue past to Fandalin',
+				index:3
+			}
+		]
+	},
+	{
+		title:'Talking to Farmer about Pigs',
+		text:"You ask the farmer for more information about the pigs.\
+    <br\><br\>'They weren’t easily scared i’ll tell you that! Takes a lot to spook em. Whatever it was must have been real scary...'\
+    <br\><br\>'I must say....i've been sleeping pretty badly the last few days I wonder if they're linked'",
+		choices:[
+			{
+				text:'Go to the stable',
+				index:4
+			}
+		]
+	}
+]
